9324f3785ad3703e67a426806b263a6c545318a0
Marius Alexandru <amarius2000@gmail.com>
2021-07-10 14:50:50 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -5,12 +5,7 @@ function getprod(data) {
     });
     $("#ButtonList_24/7").append(items.join(""));
 
-    var items2 = [];
-    $.each(data.buttons2, function(key, val) { 
-      items2.push("<a btn href='"+val.link+"'>" + val.title + "</a>");
-    });
-    $("#ButtonList_LiveEvents").append(items2.join(""));
-    
+   
   }
 
   $.getJSON('products.json',  getprod(data));
