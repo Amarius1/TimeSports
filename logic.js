d63b3d0df0c649d0fa4031265c5c2082770de161
Marius Alexandru <amarius2000@gmail.com>
2021-07-10 13:17:37 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,11 +1,11 @@
 function getprod(data) {
     var items = [];
     $.each(data.products, function(key, val) {
-      items.push("<a class='btn' href='"+val.link+"'>" + val.link + "</a>");
+      items.push("<a class='btn' href='"+val.link+"'>" + val.title + "</a>");
     });
     var tb = items.join("");
     console.log(tb);
     $("#ButtonList").append(tb);
   }
 
-  $.getJSON('https://github.com/Amarius1/TimeSports/blob/main/data.json',  getprod(data));
+  $.getJSON('products.json',  getprod(data));
