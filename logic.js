c3574ff695dfd108cbb36ac20ebbe78faef5d238
Marius Alexandru <amarius2000@gmail.com>
2021-07-10 14:20:31 +0300

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,15 +1,14 @@
 function getprod(data) {
     var items = [];
-    data.products.buttons.forEach(function(key, val) {
+    $.each(data.buttons, function(key, val) {
       items.push("<a class='btn' href='"+val.link+"'>" + val.title + "</a>");
     });
     var tb = items.join("");
     console.log(tb);
-    document.getElementById("ButtonList").appendChild(tb)
+    $("#ButtonList").append(tb);
 
 
     
   }
 
-
-  fetch('products.json').then(response => response.json()).then(data => getprod(data))
+  $.getJSON('products.json',  getprod(data));
